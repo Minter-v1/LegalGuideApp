8d08ec376df2fbbae45bb2f817cdb468cd851103
Lee minji <adf5858@naver.com>
2025-05-05 01:59:06 +0900

    Initial Commit

---
+++ LegalGuideApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LegalGuideApp
+//
+//  Created by 이민지 on 5/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LegalGuideApp/LegalGuideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegalGuideAppApp.swift
+//  LegalGuideApp
+//
+//  Created by 이민지 on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LegalGuideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
