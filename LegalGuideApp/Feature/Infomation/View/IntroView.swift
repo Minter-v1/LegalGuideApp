851c7b060bdc16bb16ad6070781efd1cee22d9ea
Minter0817 <adf5858@naver.com>
2025-05-06 01:36:56 +0900

    Feat(view) : 앱 Introduction 뷰 구현

--- LegalGuideApp/Feature/Infomation/View/IntroView.swift
+++ LegalGuideApp/Feature/Infomation/View/IntroView.swift
@@ -21,6 +21,7 @@ struct IntroView: View {
     
     
     //MARK: - 단순 데이터 나열이라 ViewModel로 빼진 않았음
+    //시스템 이미지인 "photo.artframe" 실제 디바이스에 안 보일 수 있음
     let steps: [AppIntro] = [
         AppIntro(imageName: "photo.artframe", title: "외국인 근로자를 위한 법률 가이드", description: "근로계약서 및 법류 제공 서비스에\n오신 것을 환영합니다."),
         AppIntro(imageName: "photo.artframe", title: "근로계약서 번역", description: "근로계약서를 촬영하거나 업로드하면\n자동으로 번역해 드립니다."),
@@ -31,78 +32,89 @@ struct IntroView: View {
     
     
     var body: some View {
-        //전체 뷰를 감싸는 스택
-        VStack(alignment: .center, spacing: 0) {
-            
-            //MARK: - 로고 아이콘 + 어플리케이션 이름
-            HStack(alignment: .center, spacing: 0) {
-                //TODO: - 아이콘 변경
-                Image(systemName: "photo.artframe")
-                    .frame(width: 48, height: 48)
-                    .padding(.trailing, 11)
-                //어플맄이션 이름
-                Text("LegalGuide")
-                    .font(
-                        .custom("Inter",size: 24)
-                        .weight(.bold)
-                    )
-            }
-            .padding(.bottom, 159)
-            
-            //MARK: 사진 변경
-            Image(systemName: steps[currentFrame].imageName)
-                .resizable()
-                .frame(width: 238, height: 160)
-                .padding(.bottom, 120)
-           
-            //텍스트뷰 묶은 VStack
-            VStack(alignment: .center, spacing: 6){
-                //제목
-                Text(steps[currentFrame].title)
-                    .font(
-                        .custom("Inter", size: 24)
-                        .weight(.bold)
-                    )
+        NavigationStack {
+            //MARK: - 전체 뷰
+            VStack(alignment: .center, spacing: 0) {
                 
-                //설명
-                Text(steps[currentFrame].description)
-                    .font(
-                        .custom("Inter", size: 16)
-                    )
-                    .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.55))
-                    .multilineTextAlignment(.center)
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .padding(.horizontal, 16)
-                    
-            }
-            .frame(maxWidth: .infinity, alignment: .center)
-            .padding(.bottom, 109)
-            
-            //다음 버튼
-            BlueButton(text: currentFrame == steps.count - 1 ? "시작하기" : "다음") {
-                if currentFrame < steps.count - 1 {
-                    currentFrame += 1
-                } else {
-                    isActive = true //페이지 이동
+                NavigationLink(
+                    destination: EntryView(), //이동뷰
+                    isActive: $isActive //상태변수 참조
+                ) { EmptyView() }
+                
+                
+                
+                //MARK: - 로고 아이콘 + 어플리케이션 이름
+                HStack(alignment: .center, spacing: 0) {
+                    //TODO: - 아이콘 변경
+                    Image(systemName: "photo.artframe")
+                        .frame(width: 48, height: 48)
+                        .padding(.trailing, 11)
+                    //어플리케이션 이름
+                    Text("LegalGuide")
+                        .font(
+                            .custom("Inter",size: 24)
+                            .weight(.bold)
+                        )
+                        .foregroundColor(.black) //NOTE: 색 변경 필요할 수도? 다크모드 고려
                 }
+                .padding(.bottom, 159)
+                
+                //MARK: 사진
+                //TODO: 사진 변경
+                Image(systemName: steps[currentFrame].imageName)
+                    .resizable()
+                    .frame(width: 238, height: 160)
+                    .padding(.bottom, 120)
+               
+                //MARK: - 텍스트뷰 섹션
+                VStack(alignment: .center, spacing: 6){
+                    //제목
+                    Text(steps[currentFrame].title)
+                        .font(
+                            .custom("Inter", size: 24)
+                            .weight(.bold)
+                        )
+                        .foregroundColor(.black) //NOTE: 색 변경 필요할 수도? 다크모드 고려
+                    
+                    //설명
+                    Text(steps[currentFrame].description)
+                        .font(
+                            .custom("Inter", size: 16)
+                        )
+                        .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.55))
+                        .multilineTextAlignment(.center)
+                        .lineLimit(nil)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.horizontal, 16)
+                        
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.bottom, 109)
+                
+                //다음 버튼
+                DefaultButton(text: currentFrame == steps.count - 1 ? "시작하기" : "다음") {
+                    if currentFrame < steps.count - 1 {
+                        currentFrame += 1
+                    } else {
+                        isActive = true //페이지 이동
+                    }
+                }
+                .padding(.bottom, 64)
+                
+                //하단 indicator
+                Indicator(currentIndex: currentFrame)
+                    .padding(.bottom, 15)
+                
+                
+                
+                
             }
-            .padding(.bottom, 64)
-            
-            //하단 indicator
-            Indicator(currentIndex: currentFrame)
-                .padding(.bottom, 15)
-            
-            
-            NavigationLink(
-                destination: EntryView(), //이동뷰
-                isActive: $isActive //상태변수 참조
-            ) { EmptyView() }
-            
+            .padding(.horizontal, 30)
+            .padding(.top, 57)
+            .background(.white)
+            //.animation(.easeIn,value: currentFrame)
         }
-        .padding(.horizontal, 30)
-        .padding(.top, 57)
-//        .animation(.easeIn,value: currentFrame)
+        
         
        
     }
