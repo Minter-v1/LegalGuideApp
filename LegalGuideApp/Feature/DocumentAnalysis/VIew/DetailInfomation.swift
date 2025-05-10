6a8c8ef89955fbffda0121c57d39e7a1bf40eff6
Lee minji <adf5858@naver.com>
2025-05-11 01:34:16 +0900

    Feat(component, ui) : 공통 헤더 구현

---
+++ LegalGuideApp/Feature/DocumentAnalysis/VIew/DetailInfomation.swift
@@ -0,0 +1,18 @@
+//
+//  DetailInfomation.swift
+//  LegalGuideApp
+//
+//  Created by 이민지 on 5/11/25.
+//
+
+import SwiftUI
+
+struct DetailInfomation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailInfomation()
+}
