9c6441d520a124951a968c68ff8bb908667efd2f
yxxunseo <osope@naver.com>
2025-05-15 22:16:36 +0900

    Feat(view) : 회원가입 개인정보 화면 구현

--- LegalGuideApp/Feature/Auth/View/SignupInfoView.swift
+++ LegalGuideApp/Feature/Auth/View/SignupInfoView.swift
@@ -14,46 +14,51 @@ struct SignupInfoView: View {
     @State private var phone: String = ""
     @State private var id: String = ""
     var body: some View {
-        VStack(spacing: 32) {
-            LogoAppName()
-                .padding(.bottom, 32)
-            
-            VStack(spacing: 16) {
-                Text("회원가입")
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(.black)
+        NavigationStack {
+            VStack(spacing: 32) {
+                LogoAppName()
+                    .padding(.bottom, 32)
                 
-                Text("계정을 만들어 서비스를 이용하세요")
-                    .font(.system(size: 16))
-                    .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.55))
-                
-                VStack(alignment: .leading, spacing: 20) {
-                    CustomDropdown(
-
-                        title: "국적",
-                        options: ["베트남", "중국", "태국", "필리핀", "인도네시아", "기타"],
-                        selection: $nationality
-                    )
+                VStack(spacing: 16) {
+                    Text("회원가입")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.black)
                     
-                    CustomDropdown(
-                        title: "사용 언어",
-                        options: ["한국어", "베트남어", "중국어", "태국어", "필리핀어", "인도네시아어", "영어"],
-                        selection: $language
-                    )
+                    Text("계정을 만들어 서비스를 이용하세요")
+                        .font(.system(size: 16))
+                        .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.55))
                     
-                    Text("이름")
-                        .font(.subheadline)
-                    InputField(placeholder: "이름을 입력하세요", text: $name)
-                    
-                    Text("연락처")
-                        .font(.subheadline)
-                    InputField(placeholder: "연락처를 입력하세요", text: $phone)
+                    VStack(alignment: .leading, spacing: 20) {
+                        CustomDropdown(
+                            
+                            title: "국적",
+                            options: ["베트남", "중국", "태국", "필리핀", "인도네시아", "기타"],
+                            selection: $nationality
+                        )
+                        
+                        CustomDropdown(
+                            title: "사용 언어",
+                            options: ["한국어", "베트남어", "중국어", "태국어", "필리핀어", "인도네시아어", "영어"],
+                            selection: $language
+                        )
+                        
+                        Text("이름")
+                            .font(.subheadline)
+                        InputField(placeholder: "이름을 입력하세요", text: $name)
+                        
+                        Text("연락처")
+                            .font(.subheadline)
+                        InputField(placeholder: "연락처를 입력하세요", text: $phone)
+                    }
                 }
+                Spacer()
                 
-               // DefaultButton(title: "다음")
+                DefaultButton(text: "다음") {
+                }
             }
+            .padding(.horizontal, 14)
+            .padding(.bottom, 20)
         }
-        .padding(.horizontal, 14)
     }
 }
 
